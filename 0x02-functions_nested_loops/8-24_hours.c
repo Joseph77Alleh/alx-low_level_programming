42bea9ef6de0257bb6c65e54d790a0627c8b6b3f
Joseph77Alleh <josephaduozava@gmail.com>
2022-10-18 23:24:52 -0700

    changes

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,12 +10,12 @@ void jack_bauer(void)
 
 	for (hour = 0; hour <= 23; hour++)
 	{
-		for (minutes = 0; minutes <= 59; minute++)
+		for (minutes = 0; minutes <= 59; minutes++)
 		{
 			_putchar((hour / 10) + '0');
 			_putchar((hour % 10) + '0');
 			_putchar(':');
-			_putchar((minutes / 10) + '10');
+			_putchar((minutes / 10) + '0');
 			_putchar((minutes % 10) + '0');
 			_putchar('\n');
 		}
