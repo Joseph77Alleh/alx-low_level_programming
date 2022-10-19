89992011a6c0b4cc5a4c3f3b95a804283efb2ce8
Joseph77Alleh <josephaduozava@gmail.com>
2022-10-19 08:18:41 -0700

    Even Liber Abbaci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,9 +9,10 @@ int main(void)
 	unsigned long fib1 = 0, fib2 = 1, fibsum;
 	float tot_sum;
 	
-	while
+	while(1)
 	{
 		fibsum = fib1 + fib2;
+
 		if (fibsum > 4000000)
 			break;
 		if ((fibsum % 2) == 0)
