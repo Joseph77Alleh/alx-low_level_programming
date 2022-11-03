4e9bf90ac6e9afefefc3a548e757f0bce493e87b
Joseph77Alleh <josephaduozava@gmail.com>
2022-11-02 19:23:58 -0700

    task 8

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * str_checker - check if two strings are identical
+ * @s1: string_1 base address.
+ * @s2: string_2 base address.
+ * @a: left index
+ * @b: special index
+ * Return: 1 otherwise 0
+ */
+int str_checker(char *s1, char *s2, int a, int b)
+{
+	if (s1[a] == '\0' && s2[b] == '\0')
+		return (0);
+	if (s1[a] == s2[b])
+		return (str_checker(s1, s2, a + 1, b + 1));
+	if (s1[a] == '\0' && s2[b] == '*')
+		return (str_checker(s1, s2, a, b + 1));
+	if (s2[b] == '*')
+		return (str_checker(s1, s2, a + 1, b) || str_checker(s1, s2, a, b + 1));
+	return (0);
+}
+
+/**
+ * wildcmp - check if strings could be considered identical
+ * @s1: base address for string
+ * @s2: base address for string
+ * Return: 1
+ */
+int wildcmp(char *s1, char *s2)
+{
+	return (str_checker(s1, s2, 0, 0));
+}
