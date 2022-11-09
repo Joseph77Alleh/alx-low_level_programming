038849ce0779fae39262bdb5b21103ac2787a969
Joseph77Alleh <josephaduozava@gmail.com>
2022-11-08 16:02:48 -0800

    task1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,63 +1,36 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * _strlen - count array
- * @s: array of elements
- * Return: 1
+ * _strdup - returns a pointer to a newly allocated
+ * space in memory, which contains a copy of the
+ * string given as a parameter.
+ * @str:String to be copied
+ *
+ * Return: NULL in case of error, pointer to allocated
+ * space
  */
-int _strlen(char *s)
-{
-	unsigned int i;
 
-	i = 0;
-	while (s[i] != '\0') /*count character of string*/
-	{
-		i++;
-	}
-	return (1);
-}
-
-/**
- * _strcpy - copy arrays
- * @src: array of elements
- * @dest: dest array
- * Return: dest
- */
-char *_strcpy(char *dest, char *src)
-{
-	int i = 0;
-
-	while (src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-
-/**
- * _strdup - array for prints a string
- * @str: array of elements
- * Return: pointer
- */
 char *_strdup(char *str)
 {
-	char *dst;
-	unsigned int size;
+	char *cpy;
+	int index, len;
 
-	if (str == 0)
-	{
+	if (str == NULL)
 		return (NULL);
-	}
-	size = _strlen(str) + 1;
 
-	dst = (char *) malloc(size * sizeof(char));
-	if (dst == 0)
-	{
+	for (index = 0; str[index]; index++)
+		len++;
+	cpy = malloc(sizeof(char) * (len + 1));
+
+	if (cpy == NULL)
 		return (NULL);
+
+	for (index = 0; str[index]; index++)
+	{
+		cpy[index] = str[index];
 	}
-	_strcpy(dst, str);
-	return (dst);
+
+	cpy[len] = '\0';
+
+	return (cpy);
 }
